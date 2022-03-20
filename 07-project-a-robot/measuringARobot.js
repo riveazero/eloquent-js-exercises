ee4bd5fc1e1a8186a3f051c725a5e93d21254666
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-03-20 18:55:45 +0300

    Add the chapter's project code and the first exercise

---
+++ 07-project-a-robot/measuringARobot.js
@@ -0,0 +1,24 @@
+import * as aRobot from './aRobot.js';
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+
+    let steps1 = 0, steps2 = 0;
+    for (let i = 0; i < 100; i++) {
+        let state = aRobot.VillageState.random();
+        steps1 += numSteps(state, robot1, memory1);
+        steps2 += numSteps(state, robot2, memory2);
+    }
+
+    return `Number of steps:\nRobot1: ${steps1 / 100}, Robot2: ${steps2 / 100}`;
+}
+
+function numSteps(state, robot, memory) {
+    for (let turn = 0; ; turn++) {
+        if (state.parcels.length === 0) return turn;
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+    }
+}
+
+console.log(compareRobots(aRobot.routeRobot, [], aRobot.goalOrientedRobot, []));
